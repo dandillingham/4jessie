d5cc6fe72bcb274577a2773101404988b381adb5
Dan Dillingham <dan.dillingham@dunelm.com>
2022-05-29 14:25:06 +0100

    .

---
+++ my-form.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import stringComparison
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template("index.html") # this should be the name of your html file
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    first_name = request.form['fname']
+    last_name = request.form['lname']
+    date_of_birth = request.form['dob']
+
+    return "<h1>Data submitted!</h1>"
+
+if __name__ == '__main__':
+    app.run()
