7c5f5d513938c3e2f32697cc02b6c082b1a4b6b7
Dan Dillingham <dan.dillingham@dunelm.com>
2022-07-22 19:04:35 +0100

    .

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
-#docker build -t my-app .
-#docker run --rm -p 8080:8080 my-app
+# docker build -t my-app .
+# docker run --rm -p 8080:8080 my-app
+
+# sqlite3 people.db < setup.sql
+
+# .dump
+
+
+
+
 
 FROM ubuntu 
 
--- my-form.py
+++ my-form.py
@@ -12,16 +12,36 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
 
-    first_name = request.form['fname']
-    last_name = request.form['lname']
-    date_of_birth = request.form['dob']
+    connection = sqlite3.connect('people.db')
+    cursor = connection.cursor()
 
-    print (first_name, flush=True)
-    print (last_name, flush=True)
-    print (date_of_birth, flush=True)
+    my_form = request.form['my-form']
+
+    if my_form == "Insert":
+        do_insert(cursor, request.form)
+    
+    connection.commit()
+    connection.close()
+
     print (request.form, flush=True)
-
     return "<h1>Form submitted!</h1>"
+
+def do_insert(cursor, form):
+
+    cursor.execute("select max(id) from person")
+    tempid = cursor.fetchall()
+    if tempid[0][0] == None:
+        rowid = 1
+    else:
+        rowid = int(tempid[0][0])+1
+
+    first_name    = form['fname']
+    last_name     = form['lname']
+    date_of_birth = form['dob']
+    mummy         = form['mummy']
+    daddy         = form['daddy']
+
+    cursor.execute("INSERT INTO person VALUES (?, ?, ?, ?, ?, ?)", (rowid, first_name, last_name, date_of_birth, mummy, daddy))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
